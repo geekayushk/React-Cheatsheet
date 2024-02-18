65cd6e9186c36ab6e40b902d12789912b13768a3
Ayush Kumar <kumarayush0427@gmail.com>
2024-02-18 18:54:56 +0530

    Context api

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Github() {
-    const [data,setData]=useState([])
-    useEffect(()=>{
-        fetch('https://api.github.com/users/geekayushk')
-        .then(response=>response.json())
-        .then(data=>{
-            console.log(data)
-            setData(data)
-        })
-    })
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/geekayushk")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  });
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers:{data.followers} <img src={data.avatar_url} alt="picture" width={300}/></div>
-  )
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers:{data.followers}{" "}
+      <img src={data.avatar_url} alt="picture" width={300} />
+    </div>
+  );
 }
 
-export default Github
+export default Github;
