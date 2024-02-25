3b4344b55f4dfb9cf57fd6b49704433fbe6807a0
Ayush Kumar <kumarayush0427@gmail.com>
2024-02-25 17:21:31 +0530

    routes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,35 +6,35 @@ import './App.css'
 function App() {
 
   //useState()==> use to propagate the changes in UI/DOM
-  const [counter,setCounter]=useState(5)
+  const [counter, setCounter] = useState(5)
 
   // let counter=5
 
-  const addValue=()=>{
-    
+  const addValue = () => {
+
     // counter=counter+1
-    if(counter<20)
-      setCounter(counter+1)
+    if (counter < 20)
+      setCounter(counter + 1)
     else
       alert("limit reached")
   }
 
-  const removeValue=()=>{
-    if(counter>0)
-      setCounter(counter-1)
+  const removeValue = () => {
+    if (counter > 0)
+      setCounter(counter - 1)
     else
       alert("limit reached")
   }
   return (
     <>
-     <h1>Chai aur React</h1>
-     <h2>Counter Value: {counter}</h2>
+      <h1>Chai aur React</h1>
+      <h2>Counter Value: {counter}</h2>
 
-     <button
-     onClick={addValue}>Add value</button>
-     <br />
-     <button
-     onClick={removeValue}>Remove value</button>
+      <button
+        onClick={addValue}>Add value</button>
+      <br />
+      <button
+        onClick={removeValue}>Remove value</button>
     </>
   )
 }
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -36,20 +36,20 @@ import Github from './components/Github/Github.jsx'
 
 //method-2 for creating routing
 
-const router=createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
-      <Route path='user/:userid' element={<User/>}/>
-      <Route path='github' element={<Github/>}/>
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='github' element={<Github />} />
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
